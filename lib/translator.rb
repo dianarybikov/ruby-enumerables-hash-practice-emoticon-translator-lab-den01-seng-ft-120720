95101998f83bd1c6c28c611bfea9d9a818e8215b
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-28 07:59:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,9 +15,8 @@ end
 def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
     #binding.pry
-    if value[:japenese] == emoticon
+    if value[:japenese] = emoticon
       return key
-      
     end
   end
 end
