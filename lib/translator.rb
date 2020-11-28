322b19ff6109d0d60b0ce93654332c9a06087773
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-28 07:58:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+require 'pry'
+
+def load_library(path)
+ final_hash = {}
+ YAML.load_file(path).each do |key, value|
+  final_hash[key] = {}
+  final_hash[key][:english] = value[0]
+  final_hash[key][:japanese] = value[1]
+end
+final_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  load_library(path).each do |key, value|
+    #binding.pry
+    if value[:japenese] == emoticon
+      return key
+      
+    end
+  end
 end
